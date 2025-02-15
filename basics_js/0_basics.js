02d271f17591fa8fc4a329e9c3974e638beef9f1
Tutul Mandal <tutulmandal703@gmail.com>
2025-02-15 18:19:49 +0000

    container config done

--- basics_js/0_basics.js
+++ basics_js/0_basics.js
@@ -1,1 +1,10 @@
 console.log("hello world");
+let accountEmail="motioncgi@gmail.com"
+const accountID=14553
+var accountPassword="123445"
+accountCity = "Jaipur"
+
+accountID=2
+
+console.log(accountID);
+
